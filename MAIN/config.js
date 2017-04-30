bfe22f6a6f8ab2c9c080e9a856aa2b718816d374
eposasdieste <eposas@dieste.com>
2017-04-30 16:56:41 -0500

    edited config

--- MAIN/config.js
+++ MAIN/config.js
@@ -1,6 +1,6 @@
 var config = {
     title : 'AWC34261',
     sections : ['SPANISH'],
-    ad_sizes : [  ],
+    ad_sizes : ['160x600','300x250','300x600','320x50','728x90'],
     download_zip_enabled : false
 }
